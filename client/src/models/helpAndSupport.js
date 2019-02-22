63d7a8993f37e56423fb70c096d8d18d1a2953b0
Davide Venditti <davide_335@msn.com>
2019-02-22 16:41:14 +0000

    add userformpage

--- client/src/models/helpAndSupport.js
+++ client/src/models/helpAndSupport.js
@@ -1,4 +1,4 @@
-
+const links = require('../../../server/assets/linksHelp.json')
 
 const HelpAndSupport = function(container, button){
   this.container = container
@@ -8,8 +8,39 @@ const HelpAndSupport = function(container, button){
 
 HelpAndSupport.prototype.bindEvents = function () {
   this.button.addEventListener('click', (evt) => {
+    this.container.innerHTML = " "
 
-    this.container.innerHTML = "HELP AND SUPPORT INFORMATION"
+    const divForLinksHelp = document.createElement('div')
+    divForLinksHelp.id = 'helps-links'
+
+    const description = document.createElement('h3')
+    description.textContent = "Major councelling services in scotland"
+    divForLinksHelp.appendChild(description)
+
+
+
+
+    this.container.appendChild(divForLinksHelp)
+    const list = document.createElement('ul')
+    const cancerResearchUk = document.createElement('a')
+    cancerResearchUk.href = "https://www.cancerresearchuk.org/about-cancer/coping/emotionally/talking-about-cancer/counselling/how-to-find-a-counsellor"
+    cancerResearchUk.textContent = "Cancer Research UK"
+    list.appendChild(cancerResearchUk)
+
+    const cancersupportScotland = document.createElement('a')
+    cancersupportScotland.href = "https://www.cancersupportscotland.org/services/talking-therapy/"
+    cancersupportScotland.textContent = "Cancer support Scotland"
+    list.appendChild(cancersupportScotland)
+
+    const pescanScotCouncelling = document.createElement('a')
+    pescanScotCouncelling.href = "https://www.pancanscot.org/support/counselling-therapies/"
+    pescanScotCouncelling.textContent = "Pescan Scot Councelling"
+    list.appendChild(pescanScotCouncelling)
+
+
+    divForLinksHelp.appendChild(list)
+
+
   })
 };
 
