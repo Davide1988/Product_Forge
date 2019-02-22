629773448085e67b7bd332a88eddf5f69cd4c0f2
Davide Venditti <davide_335@msn.com>
2019-02-22 17:19:57 +0000

    add new user

--- client/src/models/userForm.js
+++ client/src/models/userForm.js
@@ -19,7 +19,37 @@ UserForm.prototype.bindEvents = function () {
 
 UserForm.prototype.createForm = function (div) {
 
+  const dataForm = document.createElement('form')
+  const nameInputLabel = document.createElement('label')
+  nameInputLabel.textContent = "Name :"
+  const nameInput = document.createElement('input')
+  dataForm.appendChild(nameInputLabel)
+  dataForm.appendChild(nameInput)
 
+
+  const ageLabel = document.createElement('label')
+  ageLabel.textContent = "Age :"
+  const ageInput = document.createElement('input')
+  dataForm.appendChild(ageLabel)
+  dataForm.appendChild(ageInput)
+
+
+
+
+  const sexSelector = document.createElement('select')
+  dataForm.appendChild(sexSelector)
+
+  const male = document.createElement("option")
+  male.textContent = "Male"
+  sexSelector.appendChild(male)
+
+  const female = document.createElement("option")
+  female.textContent = "Female"
+  sexSelector.appendChild(female)
+
+
+
+  div.appendChild(dataForm)
 
 };
 
